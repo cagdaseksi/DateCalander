72070e124cfb23a1a1fdd7e245d2d67dd6077e37
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-07 21:46:25 +0300

    initial commit

--- DateCalendar/ViewController.swift
+++ DateCalendar/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let date = Date()
+        
+        let calendar = Calendar.current
+        
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        let second = calendar.component(.second, from: date)
+        
+        print("\(hour):\(minute):\(second)")
+
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        
+        print("\(day).\(month).\(year)")
+        
     }
 
 
